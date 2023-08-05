592edeb194527c4686620673d95c1f529df512de
gitandone <gitandone@gmail.com>
2023-08-05 15:54:50 +0800

    adding proxy, removing CORS headers

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
+const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+const apiUrl = "https://translation.googleapis.com/v2/projects/webtranslateapp/translations/text:translate";
+
 document.getElementById("translationForm").addEventListener("submit", async (event) => {
-    event.preventDefault();
+  event.preventDefault();
 
-    const text = document.getElementById("inputText").value;
-  
-    const translate = await fetch("https://translation.googleapis.com/v2/projects/webtranslateapp/translations/text:translate", {
-      method: "POST",
-      headers: {
-        "Authorization": "Bearer AIzaSyBQ1uv1kQobCdpi68jKsHJ2UJqr-sBt2u4",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        "source": "en",
-        "target": "es",
-        "text": text
-      })
-    });
-  
-    const response = await translate.json();
-  
-    console.log(response);
+  const text = document.getElementById("inputText").value;
 
-    document.getElementById("outputText").value = response.data.translations[0].translatedText;
+  const translate = await fetch(proxyUrl + apiUrl, {
+    method: "POST",
+    headers: {
+      "Authorization": "Bearer YOUR_API_KEY",
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "source": "en",
+      "target": "es",
+      "text": text
+    })
   });
+
+  const response = await translate.json();
+
+  console.log(response);
+
+  document.getElementById("outputText").value = response.data.translations[0].translatedText;
+});
